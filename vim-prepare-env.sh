14ad144babc09b7b69597f0e4057844b350be9b7
witcher89 <andurilx@gmail.com>
2017-05-23 22:01:08 +1000

    Basic support for iOS

--- vim-prepare-env.sh
+++ vim-prepare-env.sh
@@ -9,6 +9,29 @@
 source ./inc/colors.inc
 
 youCompleteMeDir="$HOME/.vim/bundle/YouCompleteMe"
+operatingSystem=""
+package_manager=""
+
+detect_os(){
+	echo -e "[${CYAN}INFO${RESTORE}] Detecting operating System...";
+	operatingSystem="$OSTYPE"
+	echo -e "[${CYAN}INFO${RESTORE}] Detected: ${operatingSystem}"
+
+	local darwin="darwin"
+	local gnu="gnu"
+	if test "${operatingSystem#*$darwin}" != "$operatingSystem"
+	then
+		package_manager="homebrew"
+		echo $package_manager
+		hash brew 2>/dev/null || { echo >&2 "Homebrew package manager required but it's not installed.  Aborting."; exit 1; }
+	elif test "${operatingSystem#*$gnu}" != "$operatingSystem" 
+	then
+		package_manager="apt-get"
+	else 
+		echo -e "[${CYAN}INFO${RESTORE}] Your OS is not supported :("
+		exit 1
+	fi
+}
 
 create_directory(){
 	local path_to_directory=$1;
@@ -50,6 +73,43 @@ install_vim_plugins(){
 	vim +PluginInstall +qall
 }
 
+install_you_complite_me(){
+	echo "kurwa"
+	install_ycm_pre_requirements
+	echo "kurwa"
+	compile_you_complete_me
+	install_ycm_extra_conf
+}
+
+install_ycm_pre_requirements(){
+	echo $package_manager
+	echo -e "[${CYAN}INFO${RESTORE}] Installing prerequirements for YouCompleMe using $package_manager package manager"
+	case $package_manager in
+		apt-get) 
+			ycm_gnu_pre_requirements;;
+		homebrew)
+			ycm_ios_pre_requirements;;
+		*)
+			echo -e "[${CYAN}INFO${RESTORE}] FAIL: couldn't recognize package manager"
+			exit 1;;
+	esac
+}
+
+ycm_gnu_pre_requirements(){
+	sudo apt-get install build-essential cmake
+	sudo apt-get install python-dev python3-dev
+	sudo apt-get install golang
+	sudo apt-get install ninja-build
+}
+ycm_ios_pre_requirements(){
+	brew install gcc@6 
+	brew install cmake
+	brew install python
+	brew install python3
+	brew install golang
+	brew install ninja
+}
+
 compile_you_complete_me(){
 	local goSupport="--gocode-completer"
 	local cFamilySupport="--clang-completer"
@@ -59,13 +119,6 @@ compile_you_complete_me(){
 	./install.py ${cFamilySupport} ${goSupport}
 	popd
 	echo -e `pwd`
-}
-
-install_ycm_pre_requirements(){
-	sudo apt-get install build-essential cmake
-	sudo apt-get install python-dev python3-dev
-	sudo apt-get install golang
-	sudo apt-get install ninja-build
 }
 
 install_ycm_extra_conf(){
@@ -78,21 +131,32 @@ install_ycm_extra_conf(){
 	fi
 }
 
-install_you_complite_me(){
-	install_ycm_pre_requirements
-	compile_you_complete_me
-	install_ycm_extra_conf
+detect_vim(){
+	hash vim 2>/dev/null || { echo >&2 "Vim required but it's not installed.}";} 
 }
 
 install_vim(){
-	sudo apt-get install vim-gnome
+	echo -e "[${CYAN}INFO${RESTORE}]Should I install recommended CLI vim y/n ?"
+	read answer
+	if [ "$answer" == "y" ]; then
+		case $1 in
+			homebrew)
+				brew install vim --with-override-system-vi;;
+			apt-get)
+				sudo apt-get install vim-gnome;;
+			*)
+				exit 1;;
+		esac
+	fi
 	local vimrc="vimrc"
 	cp ${vimrc} ${HOME}/.${vimrc}
 }
 
-install_vim
-set_up_vundle ~/.vim
-set_up_powerline_fonts `pwd`
-install_vim_plugins
+detect_os
+#detect_vim
+#install_vim ${package_manager}
+#set_up_vundle ~/.vim
+#set_up_powerline_fonts `pwd`
+#install_vim_plugins
 install_you_complite_me
 echo -e $?
